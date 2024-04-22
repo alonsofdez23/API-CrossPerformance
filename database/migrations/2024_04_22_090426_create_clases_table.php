c7c16a99a9f896c1895265c6404eecac0c0899dd
alonsofdez23 <alonsobs23@gmail.com>
2024-04-22 09:22:40 +0200

    add model/migration clases

---
+++ database/migrations/2024_04_22_090426_create_clases_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clases', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('monitor_id')->constrained('users');
+            $table->foreignId('entreno_id')->nullable()->constrained('entrenos');
+            $table->timestamp('fecha_hora');
+            $table->smallInteger('vacantes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clases');
+    }
+};
